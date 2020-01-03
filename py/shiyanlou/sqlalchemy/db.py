cd53efdbaff1b5422d6d4603085ce8dd3fb324fe
ttyhtg <zhm20081101@gmail.com>
2020-01-03 22:36:45 +0800

    py

---
+++ py/shiyanlou/sqlalchemy/db.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine,Column,Integer,String,ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship,backref
+
+engine = create_engine('mysql://root@localhost/study?charset=utf8')
+Base = declarative_base(engine)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer,primary_key=True)
+    name = Column(String(64),unique=True,nullable=False)
+    email = Column(String(64),unique=True,nullable=False)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.name)
+
+class Course(Base):
+    __tablename__ = 'course'
+    id = Column(Integer,primary_key=True)
+    name = Column(String(64))
+    user_id = Column(Integer,ForeignKey('user.id',ondelete='CASCADE'))
+    user = relationship('User',
+            backref=backref('course',cascade='all,delete-orphan'))
+
+    def __repr__(self):
+        return '<Course: {}>'.format(self.name)
+
+if __name__ == '__main__':
+#使用声明基类的 metadata 对象的create——all 方法创建数据表
+    Base.metadata.create_all()
