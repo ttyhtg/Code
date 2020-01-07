98a66810c5b22c5374b950262a9d060684fe5bbb
ttyhtg <zhm20081101@gmail.com>
2020-01-07 12:59:40 +0800

    stack

--- py/shiyanlou/suanfa/stack.py
+++ py/shiyanlou/suanfa/stack.py
@@ -1,13 +1,13 @@
 class Stack(object):
-    def __init__(self,limit=10);
+    def __init__(self,limit=10):
         self.stack = [] #存放元素
         self.limit = limit #栈容量极限
 
     def push(self,data):
 #判断栈是否溢出
-    if len(self.stack) >= self.limit:
-        raise IndexError("超出栈容量极限")
-    self.stack.append(data)
+        if len(self.stack) >= self.limit:
+            raise IndexError("超出栈容量极限")
+        self.stack.append(data)
 
     def pop(sefl):
         if self.stack:
@@ -15,3 +15,17 @@ class Stack(object):
         else:
 #空栈不能被弹出元素
             raise IndexError('pop from an empty stack')
+
+    def peek(self):
+#查看栈的栈顶元素（最上面的元素）
+        if self.stack:
+            return self.stack[-1]
+
+    def is_empty(self):
+#查看堆栈最上面的元素
+        return not bool(self.stack判断栈是否溢)
+
+    def size(self):
+#返回栈的大小
+        return len(self.stack)
+
